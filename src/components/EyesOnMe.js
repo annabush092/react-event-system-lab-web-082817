b734a67602d083a52103748d01a693e2f2ffee8f
annabush092 <annabush092@gmail.com>
2017-10-31 15:57:37 -0400

    works in browser, tests not passing

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,20 @@
 // Code EyesOnMe Component Here
+import React from 'react';
+
+export default class EyesOnMe extends React.Component {
+  handleOnFocus(ev) {
+    ev.preventDefault()
+    console.log('Good!')
+  }
+
+  handleOnBlur(ev) {
+    ev.preventDefault()
+    console.log('Hey! Eyes on me!')
+  }
+
+  render() {
+    return (
+        <button onFocus={this.handleOnFocus} onBlur={this.handleOnBlur}>Click Me!</button>
+    )
+  }
+}
